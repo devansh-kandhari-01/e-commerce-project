5af54cb307a50536a1a666f32276ad27fd7b9f3c
devansh-kandhari-01 <devanshkandhari01@gmail.com>
2026-04-17 00:01:50 +0530

    Fix Swagger HTTPS server URL

--- src/main/java/com/companyname/ecommerce/ECommerceApplication.java
+++ src/main/java/com/companyname/ecommerce/ECommerceApplication.java
@@ -1,9 +1,12 @@
 package com.companyname.ecommerce;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(servers = @Server(url = "https://e-commerce-project-production-6c4e.up.railway.app"))
 public class ECommerceApplication {
 
     public static void main(String[] args) {
